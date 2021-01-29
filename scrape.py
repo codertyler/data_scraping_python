1f5f92013a0a1378702aa2c878488027a50a96bf
codertylerasai@gmail.com <codertylerasai@gmail.com>
2021-01-28 21:12:09 -0700

    added cibc

--- scrape.py
+++ scrape.py
@@ -1,3 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import urllib3
 
+http = urllib3.PoolManager()
+
+cibc_url = "https://www.cibc.com/en/personal-banking/credit-cards/all-credit-cards.html"
+
+response = http.request('GET', cibc_url)
+soup = BeautifulSoup(response.data, 'lxml')
+
+match = soup.find('h3', class_="product-title")
+print(match)
